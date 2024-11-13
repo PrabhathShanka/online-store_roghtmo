579f963be2cd13cff1081549012d584115693a85
Prabhath Madushanka <143389126+PrabhathShanka@users.noreply.github.com>
2024-11-13 10:53:08 +0530

    update

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -131,10 +131,10 @@ class ProductController extends Controller
 
 
 
-    public function update(Request $request, $id)
+    public function update(Request $request, Product $product)
     {
         // Retrieve the product and count existing additional images
-        $product = Product::findOrFail($id);
+        // $product = Product::findOrFail($id);
         $existingImageCount = $product->images()->count();
 
         // Calculate how many additional images can be uploaded (max 5 total)
